bb47329b34ad738a2b2ba2d476e537c9c4219bc5
uknable <uknable@hotmail.com>
2019-10-08 15:46:30 +1100

    hello

--- game_world.gd
+++ game_world.gd
@@ -5,7 +5,7 @@ func _on_HTTPRequest_request_completed(result, response_code, headers, body):
 	print(json.result)
 
 func _on_httpButton_pressed():
-	$HTTPRequest.request("https://sleepy-sands-19230.herokuapp.com/hello/bill")
+	$HTTPRequest.request("https://sleepy-sands-19230.herokuapp.com")
 
 func _on_osButton_pressed():
 	if (OS.has_touchscreen_ui_hint()):
