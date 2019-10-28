daf3d62047b77779825b3c3e251a167d64c41517
uknable <uknable@hotmail.com>
2019-10-28 15:25:23 +1100

    add filters and objects

--- puzzle_scene.gd
+++ puzzle_scene.gd
@@ -7,40 +7,43 @@ const letters = [
 	"N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
 ]
 
-var puzzle_car_texture = preload("res://.import/puzzle_car.png-4bdaefad055c51c33b48dc3e99b2a3e0.stex")
-var puzzle_lights_texture = preload("res://.import/puzzle_lights.png-41638f92209b4d26bc4614a9b416559a.stex")
-var puzzle_tree_texture = preload("res://.import/puzzle_tree.png-37364b9cd1e713e6c6bbec9119470c59.stex")
+var object_car = preload("res://.import/object_car.png-022aa2099fc851e094ad375a17503716.stex")
+var object_lights = preload("res://.import/object_lights.png-4936233f92d788303334be3d3e49151f.stex")
+var object_tree = preload("res://.import/object_tree.png-58844deec650c0cec5a6d0500edc4e5b.stex")
+var object_bus = preload("res://.import/object_bus.jpg-20a1c2014573cc1968d74f5b796e3a62.stex")
 
-var puzzles = { 
-		"puzzle_car": [
-			"car",
-			"cataracts",
-			puzzle_car_texture
-		],
-		"puzzle_lights": [
-			"lights",
-			"glaucoma",
-			puzzle_lights_texture
-		], 
-		"puzzle_tree": [
-			"tree",
-			"retinography",
-			puzzle_tree_texture
-		]
-}
+var filter_macular = preload("res://.import/filter_macular_degen.png-dca601ebfdb15fde30ee3084c4906d9c.stex")
+var filter_retinopathy = preload("res://.import/filter_retinopathy.png-030e16ad7853741bfac2bb543650402c.stex")
+var filter_glaucoma = preload("res://.import/filter_glaucoma.png-7584f9d8180d8bf9face0b72050f5f97.stex")
+var filter_retinitis = preload("res://.import/filter_retinitis.png-ea84f1c3fe90cfad200933f86d03aa33.stex")
 
-var puzzle
+var objects = [
+	[object_car, "car"],
+	[object_lights, "traffic lights"],
+	[object_tree, "tree"],
+	[object_bus, "bus"]
+]
+
+var filters = [
+	[filter_macular, "macular degeneration"],
+	[filter_retinopathy, "retinopathy"],
+	[filter_glaucoma, "glaucoma"],
+	[filter_retinitis, "retinitis"]
+]
+
+var filter
+var object
 
 func _ready():
 
-	var puzzleTextures = puzzles.keys()
-	print(puzzleTextures)
 	randomize()
-	var puzzleTextureName = puzzleTextures[randi() % puzzleTextures.size()]
-	print(puzzleTextureName)
-	puzzle = puzzles.get(puzzleTextureName)
-	$PuzzleTexture.set_texture(puzzle[2])
-	$ImpairmentName.text = puzzle[1]
+	
+	object = objects[randi() % objects.size()]
+	$PuzzleTexture.set_texture(object[0])
+
+	filter = filters[randi() % objects.size()]
+	$FilterTexture.set_texture(filter[0])
+	$ImpairmentName.text = (filter[1])
 
 	network.inProgress(Vector2(game_manager.xPos, game_manager.yPos))
 
@@ -86,14 +89,12 @@ func _on_BackButton_pressed():
 	get_tree().change_scene("res://board_state.tscn")
 
 func _on_SubmitButton_pressed():
-	if ($TextEdit.text.to_lower() == puzzle[0]):
-		$WhatIsLabel.text = puzzle[0].to_upper()
+	if ($TextEdit.text.to_lower() == object[1]):
+		$WhatIsLabel.text = object[1].to_upper()
 		print("Solved")
 		$Feedback.show()
 		$SubmitButton.hide()
 		network.updateSolved(Vector2(game_manager.xPos, game_manager.yPos))
-		#network.updateProgress(Vector2(game_manager.xPos, game_manager.yPos))
-
 		
 	else:
 		print("incorrect")
